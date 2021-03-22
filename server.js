54d3636eb4713ee8f037884b37bc90e4cd3f7e92
Ajay <ajaykanyal11@gmail.com>
2021-03-22 17:24:30 +0530

    Resolving Error

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import api from './controllers/api.js';
 const db = knex({
     client: 'pg',
     connection: {
-        host: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         ssl: true
     }
 });
